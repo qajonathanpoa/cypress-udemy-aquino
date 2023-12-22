0975fa4fa353fb7befa5b41edd459fc607b1410c
Jonathan da Silva Cavalcanti (Externo) <jonathan.cavalcanti_externo@safepag.com>
2023-12-22 17:50:08 -0300

    Testes de laços com wrap

--- cypress/e2e/select.cy.js
+++ cypress/e2e/select.cy.js
@@ -16,12 +16,21 @@ it("validar select combo produto por text value 1grauincomp", function (){
 })
    
     it.only("validar select combo pelos seus indices Cursos e Mentoria", function (){
-   cy.get('#formEsportes')
-  .select([1,2])
-  .invoke('val')
-  .should('deep.equal', ['futebol', 'Corrida'])
-       
+//    cy.get('#formEsportes')
+//   .select([1,2])
+//   .invoke('val')
+//   .should('deep.equal', ['futebol', 'Corrida'])
+    cy.get('#formEsportes')
+    .select([1,2])
+       cy.get('#formEsportes').then($el =>{
+            cy.wrap($el).should('contain.text', 'Futebol')
+            .should('contain.text','Corrida')
+            .should('contain.text','Natacao')
+            
+            expect($el.val()).to.have.length(2)
+             expect($el.val()).to.have.members(['futebol', 'Corrida'])
 
     })
+})
 
 })
