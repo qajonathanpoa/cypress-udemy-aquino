856959f7e94c9c01968dbe98c6e258de3bbd0c06
unknown <qajonathanpoa@gmail.com>
2024-02-17 13:32:51 -0300

    Atualizada versao cypress

--- cypress/e2e/iframe.cy.js
+++ cypress/e2e/iframe.cy.js
@@ -1,26 +1,54 @@
 /// <reference types="cypress" />
 
 describe('Teste de Login com Cypress', () => {
-    it('Logar com sucesso', () => {
-      // 1. Acessar a url
-      cy.visit('https://wcaquino.me/cypress/componentes.html');
+
+  beforeEach (() =>{
+     // 1. Acessar a url
+     cy.visit('https://wcaquino.me/cypress/componentes.html');
   
-      // 2. Clicar sobre o elemento
-      cy.get('#formCadastrar').click();
+  })
+    it('Deve preencher o campo texto com frame ', () => {
+     cy.get('#frame1').then(iframe =>{
+     const body = iframe.contents().find('body')
+      cy.wrap(body).find('#tfield').type('funciona')
+     })
+      
+    });
+
+    it('Deve digitar o texto "goianinho" em um campo de texto dentro de um iframe e fazer a asserção do texto digitado', () => {
+      const textoDigitado = 'goianinho';
   
-      // 3. Validar o texto do alerta com método stub getCall
-      cy.window().then((win) => {
-        const alertStub = cy.stub(win, 'alert').as('alertStub');
+      // Encontrar o iframe e acessar seu conteúdo
+      cy.get('#frame1').then(($iframe) => {
+        const iframeContent = $iframe.contents().find('body');
   
-        // 4. Preencher nome com texto Jonathan
-        cy.get('#formNome').type('Jonathan');
+        // Encontrar o campo de texto dentro do iframe e digitar o texto
+        cy.wrap(iframeContent).find('#tfield').type(textoDigitado)
+        .should('have.value', textoDigitado);
+      });
   
-        // 5. Clicar sobre o elemento
-        cy.get('#formCadastrar').click().then(() => {
-          // 6. Validar o texto do alerta com método stub getCall
-          expect(alertStub.getCall(0)).to.be.calledWith('Sobrenome eh obrigatorio');
-        });
+    });
+
+    it.only('Deve digitar o texto "goianinho" no campo de texto, fazer a asserção do texto digitado, clicar no botão e fazer a asserção do alerta', () => {
+      const textoDigitado = 'goianinho';
+      const alertText = 'Click OK!';
+
+      cy.visit('https://wcaquino.me/cypress/frame.html');
+  
+      // Encontrar o campo de texto e digitar o texto
+      cy.get('#tfield').type(textoDigitado);
+  
+      // Fazer a asserção do texto digitado
+      cy.get('#tfield').should('have.value', textoDigitado);
+  
+      // Encontrar e clicar no botão
+      cy.get('#otherButton').click();
+  
+      // Fazer a asserção do alerta
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal(alertText);
       });
     });
+  
   });
   
